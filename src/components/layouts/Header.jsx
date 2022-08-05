9e1e05e2d8ded5d611976b08eff9a408ed7bca37
dylan <dylanfruit@outlook.fr>
2022-08-05 17:11:25 +0200

    first commit

---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import argentBankLogo from "../../assets/images/argentBankLogo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUserCircle,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+const Header = () => {
+  return <div></div>;
+};
+
+export default Header;
