44fed8ebe4676b829667b91e0916e88440523ad6
dylan <dylanfruit@outlook.fr>
2022-09-04 09:17:20 +0200

    Modif css et responsive

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Account = ({ title, amount, amountDesc }) => {
   return (
     <section className="account">
-      <div className="content-wrapper">
+      <div className="account-content-wrapper">
         <h3 className="account-title">{title}</h3>
         <p className="account-amount">{amount}</p>
         <p className="account-amount-description">{amountDesc}</p>
