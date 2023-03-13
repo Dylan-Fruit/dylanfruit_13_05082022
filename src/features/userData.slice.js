3cbcf855ffdfc023132b1e8e29da35541d53b427
Dylan Fruit <74901060+Dylan-Fruit@users.noreply.github.com>
2023-03-13 14:16:14 +0100

    Update userData.slice.js

--- src/features/userData.slice.js
+++ src/features/userData.slice.js
@@ -3,8 +3,8 @@ import { createSlice } from "@reduxjs/toolkit/";
 export const userDataSlice = createSlice({
   name: "userData",
   initialState: {
-    userName: "",
-    password: "",
+    userName: "admin",
+    password: "password",
     rememberMe: false,
     userToken: "",
     firstName: "",
